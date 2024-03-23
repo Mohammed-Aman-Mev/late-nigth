5a36b1a5d92c298b72979196da9a7bca49153c7a
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-03-23 23:51:24 +0530

    all is well

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // require('dotenv').config({path:"./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("App is runing on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed", err);
+  });
